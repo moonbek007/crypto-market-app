fa3d40872b24a00df7bc4cce92e4a7e6659df62e
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-10 18:16:38 +0500

    feat: Add basic layout for home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <p>CoinOverview</p>
+        <p>TrendingCoins</p>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 }
