dc087c074c2ed8126d5616f4b12991bf3bfe950a
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-11 17:12:09 +0500

    feat: Add Categories component, fallback component and corresponding types

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
+
+import Categories from "@/components/home/Categories";
 import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
 import {
+  CategoriesFallback,
   CoinOverviewFallback,
   TrendingCoinsFallback,
 } from "@/components/home/fallback";
-import TrendingCoins from "@/components/home/TrendingCoins";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -19,7 +22,9 @@ export default function Home() {
       </section>
 
       <section className="w-full mt-7 space-y-4">
-        <p>Categories</p>
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
       </section>
     </main>
   );
--- types/index.d.ts
+++ types/index.d.ts
@@ -91,3 +91,11 @@ declare interface CoinDetailsData {
   };
   tickers: Ticker[];
 }
+
+declare interface Category {
+  name: string;
+  top_3_coins: string[];
+  market_cap_change_24h: number;
+  market_cap: number;
+  volume_24h: number;
+}
