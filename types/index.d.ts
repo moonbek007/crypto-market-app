052686065f7066750d5d1d46cb41e4ad81039060
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-11 13:46:02 +0500

    feat: Add DataTable component

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+declare interface DataTableColumn<T> {
+  header: React.ReactNode;
+  cell: (row: T, index: number) => React.ReactNode;
+  headClassName?: string;
+  cellClassName?: string;
+}
+
+declare interface DataTableProps<T> {
+  columns: DataTableColumn<T>[];
+  data: T[];
+  rowKey: (row: T, index: number) => React.Key;
+  tableClassName?: string;
+  headerClassName?: string;
+  headerRowClassName?: string;
+  headerCellClassName?: string;
+  bodyRowClassName?: string;
+  bodyCellClassName?: string;
+}
