e300b25c821942773bf8c187a0aea3cd7677928a
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-13 18:39:20 +0500

    feat: Add CandeStickChart to CoinOverview

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,37 +1,64 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import CandlestickChart from "../CandlestickChart";
 import { CoinOverviewFallback } from "./fallback";
 
 import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 
-const CoinOverview = async () => {
-  let coin;
-  try {
-    coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
-  } catch (error) {
-    console.error("Error fetching coin overview:", error);
+const CoinOverview = () => {
+  const [coin, setCoin] = useState<CoinDetailsData | null>(null);
+  const [coinOHLCData, setCoinOHLCData] = useState<OHLCData[] | null>(null);
+  useEffect(() => {
+    async function getCoinData() {
+      try {
+        const coinData = await fetcher<CoinDetailsData>("/coins/bitcoin", {
+          dex_pair_format: "symbol",
+        });
+        setCoin(coinData);
+        const coinDataOHLCData = await fetcher<OHLCData[]>(
+          "/coins/bitcoin/ohlc",
+          {
+            vs_currency: "usd",
+            days: 1,
+            // interval: "hourly",
+            precision: "full",
+          },
+        );
+        console.log(coinDataOHLCData);
+        setCoinOHLCData(coinDataOHLCData);
+      } catch (error) {
+        console.error("Error fetching coin overview:", error);
+      }
+    }
+    getCoinData();
+  }, []);
 
-    return <CoinOverviewFallback />;
-  }
-
-  if (!coin) return <CoinOverviewFallback />;
+  if (!coin || !coinOHLCData?.length) return <CoinOverviewFallback />;
 
   return (
     <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1 className="text-amber-50">
-            {formatCurrency(coin.market_data.current_price.usd)}
-          </h1>
+      <CandlestickChart data={coinOHLCData as OHLCData[]} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1 className="text-amber-50">
+              {formatCurrency(coin.market_data.current_price.usd)}
+            </h1>
+          </div>
         </div>
-      </div>
+      </CandlestickChart>
     </div>
   );
 };
