5a6dd7f7ea845fb5ac6b332f8b107c4cb724b35e
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-13 19:15:07 +0500

    feat: Add coins page layout and implement its table

--- types/index.d.ts
+++ types/index.d.ts
@@ -117,3 +117,36 @@ declare interface CandlestickChartProps {
   children?: React.ReactNode;
   initialPeriod?: Period;
 }
+
+declare interface CoinMarketData {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  last_updated: string;
+}
+
+interface NextPageProps {
+  params: Promise<{ [key: string]: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
